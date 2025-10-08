2a1a9ceedfad4773d73e7122149edb4bad190ef2
KAMAZ <kaura@i.ua>
2025-10-08 18:09:45 +0300

    task-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
 
+
+const coursesTitleArray = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
+
 function App() {
 
 
   return (
     <>
+      <ul>
+        {
+        coursesTitleArray.map((course, index) => <li key={index}>{course}</li>)
+        }
+      </ul>
     </>
   )
 }
