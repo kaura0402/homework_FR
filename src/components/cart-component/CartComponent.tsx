ac7a33a596eefe206922595c6d01e5ad2819213b
KAMAZ <kaura@i.ua>
2025-12-02 23:59:00 +0200

    Home Work 3 / 6

--- src/components/cart-component/CartComponent.tsx
+++ src/components/cart-component/CartComponent.tsx
@@ -1,16 +1,16 @@
 import type {FC, JSX} from "react";
 import type {ICart} from "../../models/ICart.ts";
-import {IUser} from "../../models/IUser.ts";
+import type {IUser} from "../../models/IUser.ts";
 
 class Props {
     cart: ICart
-    user: IUser
+
 }
 
-export const CartComponent: FC<Props> = ({cart, user}:Props) => {
+export const CartComponent: FC<Props> = ({cart}:Props) => {
     return (
         <div>
-            <div> {cart.total}  ID </div>
+            <div> {cart.total}  </div>
         </div>
     );
 };
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,6 @@ import {createBrowserRouter} from "react-router-dom";
 import {JSX} from "react";
 import {MainLayout} from "../layouts/MainLayout.tsx";
 import {UsersPage} from "../pages/UsersPage.tsx";
-import * as path from "node:path";
 import {CartsPage} from "../pages/CartsPage.tsx";
 
 export const routes = createBrowserRouter([
