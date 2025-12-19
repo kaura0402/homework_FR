1c8b824e81fb167a9e95c26052f729d9fad421dd
KAMAZ <kaura@i.ua>
2025-12-19 12:46:13 +0200

    hw-7

--- src/models/ITokenPair.ts
+++
@@ -1,5 +0,0 @@
-export interface ITokenPair {
-	accessToken: string;
-	refreshToken: string;
-}
-
--- src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-export const HomePage = () => {
-    return (
-        <>
-        Home Page
-        </>
-    );
-};
