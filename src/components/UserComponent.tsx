fea1c2cde383802d69589650efb199795157e9df
KAMAZ <kaura@i.ua>
2025-12-21 16:01:35 +0200

    hw-7/3useMemo

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,11 +1,12 @@
 import {type FC, memo} from "react";
 
 
-export const UserComponent: FC<{foo:()=>void}> = memo(() => {
+export const UserComponent: FC<{foo:()=>void, arr:number[]}> = memo(({arr}) => {
     console.log("Hello UserComponent!");
+    console.log(arr);
     return (
         <div>
-            memo функція, яка виключає повторне використання UserComponent67
+            memo функція, яка виключає повторне використання UserComponent
             UserComponent
         </div>
     );
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,11 +1,16 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 
 export const UsersComponent = () => {
     console.log("Hello UsersComponent");
 
     const [users, setUsers] = useState([]);
+
+    const arr:number[]=useMemo(()=>{//arr  працює, але повторно  не викликається
+        return [1,2,3,4,5,6];
+    },
+        []);
 
 const foo = useCallback( ()=>{ //Функція foo працює, але повторно UserComponent не викликається
     console.log("test");
@@ -21,7 +26,7 @@ const foo = useCallback( ()=>{ //Функція foo працює, але пов�
     return (
         <div>
             Users Component
-            <UserComponent foo={foo}/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
