c509e133a84f65e0fc4d04e6bc2f9527708167e1
KAMAZ <kaura@i.ua>
2026-01-01 01:19:59 +0200

    8-1 41.00

--- src/pages/ComplexPage.tsx
+++ src/pages/ComplexPage.tsx
@@ -20,8 +20,36 @@ if(!comments.length){
 
     }, [dispatch, users, posts, comments]);
     return (
-        <>
+        <div>
+            {users.map((user) => (
+                <div key={user.id}>
+                    {/* 1. Виводимо дані користувача */}
+                    <h2>Користувач: {user.name} 👤</h2>
 
-        </>
+                    {/* 2. Фільтруємо та виводимо його пости */}
+                    <h3>Пости:</h3>
+                    {posts
+                        .filter(post => post.userId === user.id)
+                        .map(post => (
+                            <div key={post.id} style={{ marginLeft: '20px' }}>
+                                <h4>{post.title} 📝</h4>
+
+                                {/* 3. Фільтруємо та виводимо коментарі до цього поста */}
+                                <p>Коментарі:</p>
+                                {comments
+                                    .filter(comment => comment.postId === post.id)
+                                    .map(comment => (
+                                        <div key={comment.id} style={{ marginLeft: '40px' }}>
+                                            💬 {comment.body}
+                                        </div>
+                                    ))}
+                            </div>
+                        ))}
+
+                    {/* 4. Розділювач між користувачами */}
+                    <hr />
+                </div>
+            ))}
+        </div>
     );
 };
