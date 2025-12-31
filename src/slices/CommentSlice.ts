14be2421d52cc53c41a242a57b6d8ab8fe7f593e
KAMAZ <kaura@i.ua>
2025-12-31 22:45:15 +0200

    8-1 31.28

--- src/slices/CommentSlice.ts
+++ src/slices/CommentSlice.ts
@@ -1,13 +1,23 @@
 import type {IComment} from "../models/IComment.ts";
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import {getAll} from "../services/api.service.ts";
 
 type CommentSliceType={comments:IComment[]}
 
 const initCommentSliceState: CommentSliceType =  {comments:[]};
 
+const loadComments = createAsyncThunk("loadComments", async (_, thunkAPI)=>{
+    const comments = await getAll<IComment[]>('/comments');
+    return thunkAPI.fulfillWithValue(comments);
+});
+
 export const commentSlice = createSlice({
     name: 'commentSlice',
     initialState: initCommentSliceState,
-    reducers:{},
-    extraReducers: builder => builder,
+    reducers: {},
+    extraReducers: builder => builder.addCase(loadComments.fulfilled, (state, action: PayloadAction<IComment[]>) => {
+        state.comments = action.payload;
+    })
 })
+
+export const commentActions = {...commentSlice.actions, loadComments}
--- src/slices/PostSlice.ts
+++ src/slices/PostSlice.ts
@@ -1,13 +1,22 @@
 import type {IPost} from "../models/IPost.ts";
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import {getAll} from "../services/api.service.ts";
 
 type PostSliceType ={posts:IPost[]}
 
 const initPostSliceState: PostSliceType =  {posts:[]};
 
+const loadPosts = createAsyncThunk("loadPosts", async (_, thunkAPI)=>{
+    const posts = await getAll<IPost[]>('/posts');
+    return thunkAPI.fulfillWithValue(posts);
+});
+
 export const postSlice = createSlice({
     name: 'postSlice',
     initialState: initPostSliceState,
     reducers:{},
-    extraReducers: builder => builder,
+    extraReducers: builder => builder.addCase(loadPosts.fulfilled, (state, action:PayloadAction<IPost[]>)=> {
+        state.posts = action.payload;
+    })
 })
+export const postActions = {...postSlice.actions, loadPosts}
